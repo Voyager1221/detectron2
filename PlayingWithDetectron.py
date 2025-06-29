9937056e8003daf9eeabd31369fb69af2ea0a55c
Voyager1221 <aayushbhadani111@gmail.com>
2025-06-29 11:23:35 +0000

    Detectron2 set on RunPod Network Volume

---
+++ PlayingWithDetectron.py
@@ -0,0 +1,11 @@
+import os
+import sys
+sys.path.append('./')
+
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.utils.visualizer import Visualizer
+import cv2
+
